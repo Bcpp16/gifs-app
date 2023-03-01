72df635da05b690cd58b1ccf9123b156cb8fa75c
Bruna Ceppa <brunaceppa116@gmail.com>
2023-03-01 15:45:21 -0300

    agregando enlace a la pagina del gif, con estilos

--- gif-expert-app/src/components/GifItem.jsx
+++ gif-expert-app/src/components/GifItem.jsx
@@ -1,10 +1,19 @@
-function GifItem({title, url, id }) {
+
+function GifItem({ title, url, id }) {
   return (
-    
-      <div className="column">
-        <img className="img-card" src={url} alt={title} />
-      </div>
+    <div className="column">
+      <a href={url} className="column" target="_blank">
+        <img className="img-card" src={url} alt={title}/>
+      </a>
       
+
+      <div className="container-name-gif">
+        <a className="link-name-gif" href={url} target="_blank">
+          <p className="name-gif">{title}</p>
+        </a>
+      </div>
+
+    </div>
   );
 }
 
